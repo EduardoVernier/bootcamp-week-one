86471272cdb0b64ff31fb7fcd73e0025a3a952c9
Eduardo Vernier <efvernier@gmail.com>
2016-09-14 21:56:49 -0300

    Implement empty card grid.

---
+++ app/src/main/java/com/iws/futurefaces/weekone/AlbumCollection.java
@@ -0,0 +1,67 @@
+package com.iws.futurefaces.weekone;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class AlbumCollection {
+
+    public static final List<AlbumItem> ITEMS = new ArrayList<AlbumItem>();
+    public static final Map<String, AlbumItem> ITEM_MAP = new HashMap<String, AlbumItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItem(AlbumItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.title, item);
+    }
+
+    private static AlbumItem createDummyItem(int position) {
+        return new AlbumItem(String.valueOf(position), "Item " + position, makeDetails(position), 0);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class AlbumItem {
+        public final String title;
+        public final String artist;
+        public final String details;
+        public final int thumbnail;
+
+        public AlbumItem(String title, String artist, String details, int thumbnail) {
+            this.title = title;
+            this.artist = artist;
+            this.details = details;
+            this.thumbnail = thumbnail;
+        }
+
+        @Override
+        public String toString() {
+            return artist;
+        }
+    }
+}
---
+++ app/src/main/java/com/iws/futurefaces/weekone/AlbumRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package com.iws.futurefaces.weekone;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.iws.futurefaces.weekone.AlbumListFragment.OnListFragmentInteractionListener;
+import com.iws.futurefaces.weekone.AlbumCollection.AlbumItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link AlbumItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class AlbumRecyclerViewAdapter extends RecyclerView.Adapter<AlbumRecyclerViewAdapter.ViewHolder> {
+
+    private final List<AlbumItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public AlbumRecyclerViewAdapter(List<AlbumItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).title);
+        holder.mContentView.setText(mValues.get(position).artist);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public AlbumItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.title);
+            mContentView = (TextView) view.findViewById(R.id.artist);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
