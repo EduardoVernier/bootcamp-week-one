3665e39aa5f28de9e2ff11a1016a67d0a556638f
Eduardo Vernier <efvernier@gmail.com>
2016-09-15 17:37:46 -0300

    Make detail view's layout collapsible.

--- app/src/main/java/com/iws/futurefaces/weekone/AlbumDetailActivity.java
+++ app/src/main/java/com/iws/futurefaces/weekone/AlbumDetailActivity.java
@@ -1,7 +1,7 @@
 package com.iws.futurefaces.weekone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
