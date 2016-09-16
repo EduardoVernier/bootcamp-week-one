9bc52212c7fb40446f098bcc49a0117abf5eae47
Eduardo Vernier <efvernier@gmail.com>
2016-09-16 18:59:04 -0300

    Commit icons.

--- app/src/main/java/com/iws/futurefaces/weekone/MainActivity.java
+++ app/src/main/java/com/iws/futurefaces/weekone/MainActivity.java
@@ -2,6 +2,10 @@ package com.iws.futurefaces.weekone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new AlbumListFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.toggleView:
+                Toast.makeText(this, "Skip selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
